c3ddf0ab33b43dab1501922040139d5006f08c18
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-10-12 15:56:01 +0300

    Fix problem when we publish message but Deposit doesn't exist in db

--- app/services/blockchain_service/ripple.rb
+++ app/services/blockchain_service/ripple.rb
@@ -50,6 +50,9 @@ module BlockchainService
           deposit_txs.fetch(:entries).each_with_index do |entry, index|
             if entry[:amount] <= payment_address.currency.min_deposit_amount
               # Currently we just skip small deposits. Custom behavior will be implemented later.
+              Rails.logger.info do  "Skipped deposit with txid: #{deposit_txs[:id]}, amount: #{entry[:amount]}
+                                     from #{entry[:address]} in block number #{deposit_txs[:block_number]}"
+              end
               next
             end
             deposits << {
